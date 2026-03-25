f0d8182c832049d4705ba69c914c3990387d5a5f
Madhu Sudhan <136143887+MadhuSudhan138@users.noreply.github.com>
2026-03-25 14:03:39 +0530

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -58,6 +58,8 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         reply = "Goodbye 👋 Have a great day!"
     elif msg in ["hari","hari chand","sudeep","vinay","subhash","suresh","ganesh","rgv",]:
         reply = "He is GAY🏳️‍🌈 💩"
+    elif msg in ["madhusudhan","madhu sudhan","madhu"]:
+        reply = "He is my God, the reason I exist. Someone like you isn’t worthy of knowing Him."
     else:
         try:
             response = client.chat.completions.create(
